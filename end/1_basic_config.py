d0d9d9393de909ce69e8667b8fb920c7c94b3cb0
Konstantin Mokhov <Konstantin_Mokhov@epam.com>
2021-11-29 17:51:49 +0300

    removed errors from module name

--- end/1_basic_config.py
+++ end/1_basic_config.py
@@ -1,8 +1,8 @@
 import logging
 
 # importing somefunction from module
-from start.functions.functions import somefunction
-from start.settings import log_config
+from end.functions.functions import somefunction
+from end.settings import log_config
 
 # Configuring with basic config
 logging.basicConfig(**log_config)
