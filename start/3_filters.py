396a954c5e2a2dcb8bf79ce1b75ad38d3b3a1c92
mohovkm <mohovkm@ya.ru>
2021-12-09 15:30:19 +0300

    Removed unused code form filters

--- start/3_filters.py
+++ start/3_filters.py
@@ -11,11 +11,6 @@ logging.basicConfig(format="%(asctime)s - %(message)s - %(login)s - %(appname)s"
 logger = logging.getLogger(__name__)
 logger.setLevel("INFO")
 
-# Inspect errors
-logging.info("logging message")  # will not be printed
-logger.info("some message")
-
-
 # Create filter method enreach with appname, filter login
 class ContextFilter(logging.Filter):
     def filter(self, record):
